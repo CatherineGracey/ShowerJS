4e093334187d26db35a149ab547bafd271d5b5ea
Catherine Gracey <cgracey@hotmail.com>
2017-11-19 11:41:32 +1100

    Refactor and tidy up.

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,25 @@
-var water = 2;
+var water;
 
 function sizeCanvas(){
-  var shower = 1
-  shower = document.getElementsByClassName('shower')[0];
-  var faucet = 0;
-  faucet = document.getElementsByClassName('faucet')[0];
+  var shower = document.getElementsByClassName('shower')[0];
+  var faucet = document.getElementsByClassName('faucet')[0];
   var faucetHeight = Math.ceil(faucet.offsetHeight);
   var showerHeight = shower.clientHeight;
   var waterHeight = showerHeight - faucetHeight;
-  water.style.height = waterHeight + "px";
-  var showerWidth = shower.clientWidth;
+  document.getElementsByClassName('water')[0].style.height = waterHeight + "px";
 }
 
-
+function makeWaterFall(){
+  var ctx = water.getContext('2d');
+  var width = water.clientWidth;
+  var height = water.clientHeight;
+  var midpoint = Math.round(width / 2);
+}
 
 window.onload = function(){
   water = document.getElementsByClassName('water')[0];
   sizeCanvas();
+  makeWaterFall();
 };
 
 window.onresize = sizeCanvas;
